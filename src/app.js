dd01e702ec680d4d1a126d4c0b1f03f3031cf8d7
tomernegad <tomernegad@gmail.com>
2024-12-02 12:45:52 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
-const { v4: uuidv4 } = require("uuid");
-console.log(uuidv4());
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send("this is a post request");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
