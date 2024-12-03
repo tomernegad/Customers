65d3b4408a873c65e3986c846437289340ad389a
tomernegad <tomernegad@gmail.com>
2024-12-03 11:59:13 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,15 +1,43 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION || 'your_default_connection_string';
 const app = express();
-const PORT = 3000;
+mongoose.set('strictQuery', false);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const names = [
+  { firstname: 'John', lastname: 'Doe' },
+  { firstname: 'Jane', lastname: 'Doe' },
+  { firstname: 'Jim', lastname: 'shami' },
+];
+
+app.post('/api/names', (req, res) => {
+  res.send(req.body);
+  console.log(req.body);
 });
 
-app.post("/", (req, res) => {
-  res.send("this is a post request");
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+app.get('/api/names', (req, res) => {
+  res.send(names);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(CONNECTION);
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+start();
